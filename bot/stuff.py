162adec3a0a8646928466a779de201746c943ec4
Madhusudhan127265588 <96523976+Madhusudhan127265588@users.noreply.github.com>
2022-01-13 08:16:05 +0530

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -33,8 +33,8 @@ async def start(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("SOURCE CODE", url="github.com/Madhusudhan127265588/"),
+                Button.url("DEVELOPER", url="t.me/HHHHaaaaakkkkiiiiiiiiiiiiiiii"),
             ],
         ],
     )
@@ -59,8 +59,8 @@ async def beck(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("SOURCE CODE", url="github.com/Madhusudhan127265588/"),
+                Button.url("DEVELOPER", url="t.me/HHHHaaaaakkkkiiiiiiiiiiiiiiii"),
             ],
         ],
     )
